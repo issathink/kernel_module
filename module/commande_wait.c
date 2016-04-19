ca37db966edb6d77e8c38d71cc69dcfea6ea78db
issathink <issamht1@gmail.com>
2016-04-19 18:14:57 +0200

    Com'on wait almost

--- module/commande_wait.c
+++ module/commande_wait.c
@@ -18,11 +18,12 @@ void thread_wait(struct work_struct *work_arg)
         c_ptr->ret_code = -1;
         no_proc = 0;
 	
-	pr_err("Initialisation ok dw: %d c_ptr: %d\n", (dw == NULL), (c_ptr == NULL));
+	pr_err("Initialisation ok task_id: %d, dw: %p\n", c_ptr->id, dw);
         
         no_proc = 0;
+        c_pid = 0;
         for(i=0; i<NB_MAX_PID; i++) {
-                pr_err("start boucle %d\n" + (c_ptr == NULL));
+                pr_err("start boucle %d\n", (c_ptr == NULL));
                 c_pid = (int)((int *)(c_ptr->first))[i];
                 pr_err("Well c_pid %d\n", c_pid);
                 p = find_get_pid(c_pid);
@@ -33,7 +34,11 @@ void thread_wait(struct work_struct *work_arg)
 	                        ((int *)(c_ptr->first))[i] = 0;
 		                pr_err("can't find task for pid %u\n", pid_nr(p));
 		                put_pid(p);
-		                put_task_struct(task);
+		                if(task == NULL) {
+		                        pr_info("Pourquoi c'est null non ????\n'");
+		                } else {
+		                        put_task_struct(task);
+		                }
 		                continue;
 	                }
                         /* pr_info("Waiting for pid: %d\n", c_pid); */
@@ -50,27 +55,28 @@ void thread_wait(struct work_struct *work_arg)
                                 put_pid(p);
                                 is_over = 1;
                                 wake_up(&wq_t);
+                                pr_info("Someone is not alive\n");
 		                return;
 		        }
 	                task_unlock(task);
 	                put_task_struct(task);
                 } else {
-              /* pr_info("Oh no pid: %d\n", pid); */
                         no_proc++;
                 }
                 put_pid(p);
         }
                 
         if(no_proc == NB_MAX_PID) {
-                scnprintf(tmp, BUFFER_SIZE, "No process found\n");
+                scnprintf(tmp, BUFFER_SIZE, "No process found");
 		copy_to_user((char *) c_ptr->thir, tmp, strlen(tmp)+1);
 		c_ptr->ret_code = 0;
                 is_over = 1;
                 wake_up(&wq_t);
+                pr_info("Nope\n");
                 return;
         }
         
-        INIT_DELAYED_WORK(&c_ptr->dwork, thread_wait);
+        // INIT_DELAYED_WORK(&c_ptr->dwork, thread_wait);
         pr_info("Avant le schedule_delayed_work\n");
         schedule_delayed_work(&c_ptr->dwork, 60);
         
@@ -78,11 +84,18 @@ void thread_wait(struct work_struct *work_arg)
 
 int wait_handler(struct file *fichier, wait_data *data)
 {
+        int i;
         struct work_task *wt = kmalloc(sizeof(struct work_task), GFP_KERNEL);
 // 	INIT_WORK(&wt->real_work, thread_wait);
         is_over = 0;
         INIT_DELAYED_WORK(&wt->dwork, thread_wait);
-	wt->first = data->pids;
+        
+
+        wt->first = kmalloc(sizeof(int)*NB_MAX_PID, GFP_KERNEL);
+        for(i=0; i<NB_MAX_PID; i++)
+                ((int *)wt->first)[i] = data->pids[i];
+         
+	// wt->first = data->pids;
 	wt->thir = data->buf;
 	wt->is_bg = data->is_bg;
 	pr_info("wait_handler\n");
