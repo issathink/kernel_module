5b2940d79f8540e53b9ee0a8ddc76d68c9b0812c
Issa <issamht1@gmail.com>
2016-05-21 18:53:53 +0200

    Update commande_fg.c

--- module/commande_fg.c
+++ module/commande_fg.c
@@ -4,10 +4,8 @@ void thread_fg(struct work_struct *work_arg)
 {
 	struct work_task *c_ptr = container_of(work_arg, struct work_task,
 					       real_work);
-	pr_info("Avant que j'attende le type is_over = %d (id: %d)\n", c_ptr->is_over, c_ptr->id);
 	if (!c_ptr->is_over)
 		wait_event(glbl->wqh, c_ptr->is_over);
-	pr_info("J'ai fini d'attendre chaine de retour : %s\n", c_ptr->tmp_buf);
 	copy_to_user((char *) c_ptr->thir, c_ptr->tmp_buf, strlen(c_ptr->tmp_buf)+1);
 	c_ptr->ret_code = 0;
 }
@@ -24,7 +22,6 @@ int fg_handler(struct file *fichier, struct fg_data *data)
 	mutex_lock(&glbl->mut);
 	list_for_each_entry(pos, &(glbl->head), list) {
 		if (pos->id == data->cmd_id) {
-			pr_info("Trouve!\n");
 			tmp_wt = pos;
 			found = true;
 			break;
@@ -35,10 +32,8 @@ int fg_handler(struct file *fichier, struct fg_data *data)
 		scnprintf(tmp, BUFFER_SIZE, "Task with id: %d not found\n", data->cmd_id);
 		copy_to_user(data->buf, tmp, strlen(tmp)+1);
 	} else {
-		pr_info("Avant de supp : %d\n", tmp_wt->id);
 		list_del(&tmp_wt->list); /* Supprimer le work de liste des taches en bg */
 		tmp_wt->thir = data->buf;
-		pr_info("Apres supp : %d\n", tmp_wt->id);
 		if (found && glbl->size > 0)
 			glbl->size--;
 	}
